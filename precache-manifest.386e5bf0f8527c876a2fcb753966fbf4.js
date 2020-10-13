756046eea050c9741a29126f868ce9e184713cd7
Appinventiv AAppinventivS-538 <vivek.rajoriya@appinventiv.com>
2020-10-13 12:44:25 +0530

    Updates

---
+++ precache-manifest.386e5bf0f8527c876a2fcb753966fbf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6fa341a929fdf604d7c5e2f35ea241",
+    "url": "/REACT-PWA-POC/index.html"
+  },
+  {
+    "revision": "7d5cffa64a7f3e77d2a2",
+    "url": "/REACT-PWA-POC/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "32c6fa865a003ba58315",
+    "url": "/REACT-PWA-POC/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d5cffa64a7f3e77d2a2",
+    "url": "/REACT-PWA-POC/static/js/2.e4f77a4f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/REACT-PWA-POC/static/js/2.e4f77a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c6fa865a003ba58315",
+    "url": "/REACT-PWA-POC/static/js/main.bfe870c5.chunk.js"
+  },
+  {
+    "revision": "7e363f120807569017a3",
+    "url": "/REACT-PWA-POC/static/js/runtime-main.81c1acdc.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,94 @@
+const cacheName = "PWA-POC-Offline";
+
+// cache files required
+this.addEventListener("install", function (event) {
+  this.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        "static/js/1.chunk.js",
+        "static/js/1.chunk.js.map",
+        "/static/js/bundle.js",
+        "/static/js/main.chunk.js",
+        "static/js/0.chunk.js",
+        "/posts",
+        "/offline.html",
+        "/"
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", function (event) {
+  // From cache when no internet connection
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.open(cacheName).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          if (response) return response;
+        });
+      })
+    );
+  }
+});
+
+// cache api call
+this.addEventListener("sync", function (event) {
+  if (event.tag === "add-post") {
+    event.waitUntil(getData("add-post"));
+  }
+  if (event.tag === "delete-post") {
+    event.waitUntil(getData("delete-post"));
+  }
+});
+
+async function addPost(addedItem) {
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(addedItem)
+  });
+}
+
+async function deletePost(postId) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+    method: "DELETE"
+  });
+}
+
+async function getData(type) {
+  var request = indexedDB.open("pwa-poc-db", 1);
+
+  request.onsuccess = function (event) {
+    let requestOnDbConnectionSuccessfull;
+    let db = event.target.result;
+    if (type === "add-post") {
+      const transaction = db.transaction(["new-post"]);
+      const objectStore = transaction.objectStore("new-post");
+      requestOnDbConnectionSuccessfull = objectStore.get("adding-postItem");
+    } else if (type === "delete-post") {
+      const transaction = db.transaction(["delete-post"]);
+      const objectStore = transaction.objectStore("delete-post");
+      requestOnDbConnectionSuccessfull = objectStore.get("postId");
+    }
+    // success from collection
+    requestOnDbConnectionSuccessfull.onerror = function (event) {
+      console.log(
+        "some error has been raised while getting data from collection",
+        event
+      );
+    };
+    // error from collection
+    requestOnDbConnectionSuccessfull.onsuccess = function (request) {
+      if (type === "add-post") {
+        addPost(request.result);
+      } else if (type === "delete-post") {
+        deletePost(request.target.result);
+      }
+      indexedDB.close();
+    };
+  };
+
+  request.onerror = function (event) {
+    console.log("while connecting to indexdb some error occured", event);
+  };
+}
